8f9fa5691a48a79576c4bf70757fba7668b024e3
zhengpj <598359997@qq.com>
2023-08-15 18:08:59 +0800

    update

--- src/maingui.py
+++ src/maingui.py
@@ -8,12 +8,12 @@ from os import path
 import main
 
 # 创建主窗口
-root = tkinter.Tk()
+win = tkinter.Tk()
 
-root.title('导表工具')
-root.geometry('800x500+200+200')
-root.minsize(800, 500)
-root.maxsize(1100, 600)
+win.title('导表工具')
+win.geometry('800x500+200+200')
+win.minsize(800, 500)
+win.maxsize(1100, 600)
 
 # 所有的xlsx文件
 xlsxRoot = path.normpath(path.join(path.dirname(__file__), '../xlsx'))
@@ -22,7 +22,7 @@ files = Utils.readFileList(xlsxRoot)
 outputRoot = path.normpath(path.join(xlsxRoot, '../output'))
 
 # 创建列表
-listbox = tkinter.Listbox(root, font=("微软雅黑", 12), height=20)
+listbox = tkinter.Listbox(win, font=("微软雅黑", 12), height=20)
 listbox.pack(fill='both', padx=20)
 for i,file in enumerate(files):
     listbox.insert(i, file)
@@ -36,7 +36,7 @@ def exportAllFunc():
         excel2Json = main.Excel2Json(filePath, outputRoot)
         excel2Json.readFile()
 
-onekeyBtn = tkinter.Button(root, text='导出所有', width=20, font=(None,15), command=exportAllFunc)
+onekeyBtn = tkinter.Button(win, text='导出所有', width=20, font=(None,15), command=exportAllFunc)
 onekeyBtn.pack(side='right')
 
 # 选中导出
@@ -51,8 +51,8 @@ def exportSelectFunc():
     excel2Json = main.Excel2Json(selectItemPath, outputRoot)
     excel2Json.readFile()
 
-singleBtn = tkinter.Button(root, text='导出选中', width=20, font=(None,15), command=exportSelectFunc)
+singleBtn = tkinter.Button(win, text='导出选中', width=20, font=(None,15), command=exportSelectFunc)
 singleBtn.pack(side='left')
 
 # 显示窗口
-root.mainloop()
+win.mainloop()
